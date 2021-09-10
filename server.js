f1ab90ac02c98d52fbfdd0ddbbb03c7ec6fe4404
leon-do <leondo123@gmail.com>
2021-09-09 20:55:07 -0400

    no emit

--- server.js
+++ server.js
@@ -3,10 +3,6 @@ const { WebSocketServer } = require('ws');
 const wss = new WebSocketServer({ port: 8080 });
 
 wss.on('connection', function connection(ws) {
-  ws.on('message', function incoming(message) {
-    console.log('received: %s', message);
-  });
-
   setInterval(() => {
     ws.send(Math.random().toString());
   }, 2000)
